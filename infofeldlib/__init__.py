e8ad445136f52253b721d61487e415352ad4b33e
Peter Hofmann <scm@uninformativ.de>
2016-08-14 09:06:27 +0200

    Move antialias handling into shadowed_text() as well

--- infofeldlib/__init__.py
+++ infofeldlib/__init__.py
@@ -1,7 +1,15 @@
 from subprocess import PIPE, Popen
 
+import cairo
+
 
 def shadowed_text(cr, args, string):
+    if not args.antialias_font:
+        fo = cairo.FontOptions()
+        fo.set_antialias(cairo.ANTIALIAS_NONE)
+        cr.set_font_options(fo)
+    cr.set_font_size(args.size_font)
+
     cr.text_path(string)
     cr.set_source_rgb(*[float(f) for f in args.color_font_shadow.split(',')])
     cr.stroke_preserve()
